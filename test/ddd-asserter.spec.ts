d7622fc63b7d246c584b043bfa11e456ea622fa4
Sébastien JAUPART <35149581+sjaupart@users.noreply.github.com>
2024-01-15 10:13:23 +0100

    tmp

--- test/ddd-asserter.spec.ts
+++ test/ddd-asserter.spec.ts
@@ -9,5 +9,22 @@ describe('DDD Asserter', () => {
         expect(report.pass).toBeTrue()
     });
 
-    
+    describe('Aggregate Rules', () => {
+        test('An aggregate must contain only value objects', () => {
+            const valueObjectDragee: Dragee = {
+                name: 'AValueObject',
+                kind_of: 'ddd/value_object',
+            }
+            const aggregateDragee: Dragee = {
+                name: 'AnAggregate',
+                kind_of: 'ddd/aggregate',
+                depends_on: {
+                    'AValueObject': ['field']
+                }
+            }
+
+            const report = asserter([valueObjectDragee, aggregateDragee])
+            expect(report.pass).toBeTrue()
+        })
+    })
 })
