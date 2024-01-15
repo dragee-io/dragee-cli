7bce77187424ca4017e6a70115e368f1f158fab6
Sébastien JAUPART <35149581+sjaupart@users.noreply.github.com>
2024-01-15 10:37:35 +0100

    feat: aggregate rules fail

--- src/ddd/ddd-asserter.ts
+++ src/ddd/ddd-asserter.ts
@@ -1,6 +1,22 @@
+const dependenciesOf: Dragee[] = (dragee: Dragee, allDragees: Dragee[]) => {
+    return Object.keys(dragee.depends_on)
+        .map(dependency => allDragees.find(dragee => dragee.name === dependency))
+}
+
+const isAggregate: boolean = (dragee: Dragee) => dragee.kind_of === 'ddd/aggregate'
+const isValueObject: boolean = (dragee: Dragee) => dragee.kind_of === 'ddd/value_object'
+
 const asserter: Asserter = (dragees: Dragee[]) => {
+    const aggregates = dragees.filter(dragee => isAggregate(dragee))
+
+    const pass = aggregates.map(aggregate => {
+        return dependenciesOf(aggregate, dragees).map(dependencyDragee => isValueObject(dependencyDragee))
+    })
+        .flatMap(result => result)
+        .filter(result => !result)
+
     return {
-        pass: true
+        pass: pass.length === 0
     }
 }
 
--- test/ddd-asserter.spec.ts
+++ test/ddd-asserter.spec.ts
@@ -10,21 +10,39 @@ describe('DDD Asserter', () => {
     });
 
     describe('Aggregate Rules', () => {
-        test('An aggregate must contain only value objects', () => {
-            const valueObjectDragee: Dragee = {
-                name: 'AValueObject',
-                kind_of: 'ddd/value_object',
-            }
-            const aggregateDragee: Dragee = {
-                name: 'AnAggregate',
-                kind_of: 'ddd/aggregate',
-                depends_on: {
-                    'AValueObject': ['field']
+        describe('An aggregate must contain only value objects', () => {
+            test('Rule passed', () => {
+                const valueObjectDragee: Dragee = {
+                    name: 'AValueObject',
+                    kind_of: 'ddd/value_object',
                 }
-            }
+                const aggregateDragee: Dragee = {
+                    name: 'AnAggregate',
+                    kind_of: 'ddd/aggregate',
+                    depends_on: {
+                        'AValueObject': ['field']
+                    }
+                }
 
-            const report = asserter([valueObjectDragee, aggregateDragee])
-            expect(report.pass).toBeTrue()
+                const report = asserter([valueObjectDragee, aggregateDragee])
+                expect(report.pass).toBeTrue()
+            })
+            test('Rule failed', () => {
+                const serviceDragee: Dragee = {
+                    name: 'AService',
+                    kind_of: 'ddd/service',
+                }
+                const aggregateDragee: Dragee = {
+                    name: 'AnAggregate',
+                    kind_of: 'ddd/aggregate',
+                    depends_on: {
+                        'AService': ['field']
+                    }
+                }
+
+                const report = asserter([serviceDragee, aggregateDragee])
+                expect(report.pass).toBeFalse()
+            })
         })
     })
 })
