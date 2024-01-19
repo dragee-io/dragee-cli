63bab4dac1102142490d34c322610f706061315f
Sébastien JAUPART <35149581+sjaupart@users.noreply.github.com>
2024-01-19 11:56:07 +0100

    refactor: simplify rule utils

--- src/ddd/ddd-rules.utils.ts
+++ src/ddd/ddd-rules.utils.ts
@@ -1,25 +1,21 @@
 export const dependenciesOf: Dragee[] = (dragee: Dragee, allDragees: Dragee[]) => {
-    if(!dragee.depends_on){
+    if (!dragee.depends_on) {
         return [];
     }
-    
+
     return Object.keys(dragee.depends_on)
         .map(dependency => allDragees.find(dragee => dragee.name === dependency))
         .filter(dragee => dragee !== undefined)
 }
 
+const kindOf: boolean = (dragee: Dragee, kind: string) => dragee.kind_of === kind
 
-export const isAggregate: boolean = (dragee: Dragee) => dragee.kind_of === 'ddd/aggregate'
-
-export const isValueObject: boolean = (dragee: Dragee) => dragee.kind_of === 'ddd/value_object'
-
-export const isEntity: boolean = (dragee: Dragee) => dragee.kind_of === 'ddd/entity'
-
-export const isEvent: boolean = (dragee: Dragee) => dragee.kind_of === 'ddd/event'
-
-export const isRepository: boolean = (dragee: Dragee) => dragee.kind_of === 'ddd/repository'
-
-export const isService: boolean = (dragee: Dragee) => dragee.kind_of === 'ddd/service'
+export const isAggregate: boolean = (dragee: Dragee) => kindOf(dragee, 'ddd/aggregate')
+export const isEntity: boolean = (dragee: Dragee) => kindOf(dragee, 'ddd/entity')
+export const isEvent: boolean = (dragee: Dragee) => kindOf(dragee, 'ddd/event')
+export const isRepository: boolean = (dragee: Dragee) => kindOf(dragee, 'ddd/repository')
+export const isService: boolean = (dragee: Dragee) => kindOf(dragee, 'ddd/service')
+export const isValueObject: boolean = (dragee: Dragee) => kindOf(dragee, 'ddd/value_object')
 
 
 
