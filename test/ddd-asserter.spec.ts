538639f600971f37391fa1c5a5791ad1638a83c4
Sébastien JAUPART <35149581+sjaupart@users.noreply.github.com>
2024-01-22 16:32:36 +0100

    create json file for each test case

--- test/ddd-asserter.spec.ts
+++ test/ddd-asserter.spec.ts
@@ -8,9 +8,14 @@ const dragee: Dragee = (name: string, kind_of: string) => ({name, kind_of})
 
 const valueObject: Dragee = (name: string) => dragee(name, 'ddd/value_object')
 const entity: Dragee = (name: string) => dragee(name, 'ddd/entity')
-const event: Dragee = (name: string) => dragee(name, 'ddd/event')
-const service: Dragee = (name: string) => dragee(name, 'ddd/service')
-const repository: Dragee = (name: string) => dragee(name, 'ddd/repository')
+
+interface TestObject {
+    dragees: Dragee[],
+    result: {
+        pass: boolean,
+        errors: string[],
+    },
+}
 
 describe('DDD Asserter', () => {
 
@@ -25,18 +30,18 @@ describe('DDD Asserter', () => {
 
         describe('An aggregate must contain only value objects, entities, or events', () => {
             test('Rule passed', () => {
-                const dragees = require(DRAGEE_FIXTURE_AGGREGATE + '/rule-passed.json')
-                const report = asserter(dragees)
-                expect(report.pass).toBeTrue()
+                const data: TestObject = require(DRAGEE_FIXTURE_AGGREGATE + '/rule-passed.json')
+                const report = asserter(data.dragees)
+                expect(report.pass).toBe(data.result.pass)
             })
             test('Rule failed', () => {
-                const dragees = require(DRAGEE_FIXTURE_AGGREGATE + '/rule-failed.json')
-                const report = asserter(dragees)
+                const data: TestObject = require(DRAGEE_FIXTURE_AGGREGATE + '/rule-failed.json')
+                const report = asserter(data.dragees)
 
-                expect(report.pass).toBeFalse()
-                expect(report.errors).toContain(
-                    'The aggregate "AnAggregate" must not have any dependency of type "ddd/service"'
-                )
+                expect(report.pass).toBe(data.result.pass)
+                data.result.errors.forEach(error => {
+                    expect(report.errors).toContain(error)
+                })
             })
         })
         describe('An aggregate must at least contains one entity', () => {
@@ -79,36 +84,34 @@ describe('DDD Asserter', () => {
 
         describe('A repository must be called only inside a Service', () => {
             test('Rule passed', () => {
-                const dragees = require(DRAGEE_FIXTURE_REPOSITORY + '/repository-rule-passed.json')
-                const report = asserter(dragees)
-                expect(report.pass).toBeTrue()
+                const data = require(DRAGEE_FIXTURE_REPOSITORY + '/rule-passed.json')
+                const report = asserter(data.dragees)
+
+                expect(report.pass).toBe(data.result.pass)
             })
             test('Rule failed', () => {
-                const dragees = require(DRAGEE_FIXTURE_REPOSITORY + '/repository-rule-failed.json');
-                const report = asserter(dragees)
+                const data: TestObject = require(DRAGEE_FIXTURE_REPOSITORY + '/rule-failed.json');
+                const report = asserter(data.dragees)
 
-                expect(report.pass).toBeFalse()
-                expect(report.errors).toContain(
-                    'The repository "ARepository1" must not be a dependency of "ddd/value_object"'
-                )
-                expect(report.errors).toContain(
-                    'The repository "ARepository2" must not be a dependency of "ddd/value_object"'
-                )
+                expect(report.pass).toBe(data.result.pass)
+                data.result.errors.forEach(error => {
+                    expect(report.errors).toContain(error)
+                })
             })
         })
     })
 
     describe('Value object rules', () => {
-        const DRAGEE_FIXTURE_VALUE_OBJECT = './ddd/value-object-rules';
+        const VALUE_OBJECT_DRAGEE_REPOSITORY = './ddd/value-object-rules';
 
         describe('Should only contains entities', () => {
-            test('Test pass', () => {
-                const dragees = require(DRAGEE_FIXTURE_VALUE_OBJECT + '/rule-passed.json')
+            test('Test passed', () => {
+                const dragees = require(VALUE_OBJECT_DRAGEE_REPOSITORY + '/rule-passed.json')
                 const report = asserter(dragees)
                 expect(report.pass).toBeTrue();
             })
-            test('Test does not pass', () => {
-                const dragees = require(DRAGEE_FIXTURE_VALUE_OBJECT + '/rule-failed.json')
+            test('Test failed', () => {
+                const dragees = require(VALUE_OBJECT_DRAGEE_REPOSITORY + '/rule-failed.json')
                 const report = asserter(dragees)
 
                 expect(report.pass).toBeFalse();
