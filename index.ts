7b0d1ab639da16917f1818d80617cdc0e1c572ed
mbernasinski <mbernasinski@hoppr.tech>
2024-10-18 16:16:51 +0200

    docs: Adding CLI commands description and summary

--- index.ts
+++ index.ts
@@ -6,6 +6,14 @@ import { reportCommandhandler } from './src/commands/report-command.handler.ts';
 
 const report = new Command('report')
     .alias('r')
+    .summary('builds asserters rules report')
+    .description(
+        'Builds asserters rules report.\n' +
+            '- Lookups dragees in [--from-dir] directory\n' +
+            '- Downloads asserters for dragees namespaces\n' +
+            '- Executes rules from asserters\n' +
+            '- Builds reports in [--to-dir] directory'
+    )
     .option('--from, --from-dir <path-to-dir>', 'directory in where to lookup for dragees', '.')
     .option(
         '--to --to-dir <path-to-dir>',
@@ -16,6 +24,14 @@ const report = new Command('report')
 
 const draw = new Command('draw')
     .alias('d')
+    .summary('builds graphers graphs models')
+    .description(
+        'Builds graphers graphs models.\n' +
+            '- Lookups dragees in [--from-dir] directory\n' +
+            '- Downloads graphers for dragees namespaces\n' +
+            '- Executes graphs from graphers\n' +
+            '- Draws models in [--to-dir] directory'
+    )
     .option('--from, --from-dir <path-to-dir>', 'directory in where to lookup for dragees', '.')
     .option(
         '--to --to-dir <path-to-dir>',
